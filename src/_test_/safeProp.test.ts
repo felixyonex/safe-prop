cad402f8ab7a5754dedf407b840265b63e14d578
Felix Wu <felixwudev@gmail.com>
2019-10-12 19:09:50 +1100

    Optimise code; add more modes and return type

--- src/_test_/safeProp.test.ts
+++ src/_test_/safeProp.test.ts
@@ -1,6 +1,67 @@
-import { SafeProp } from ".."
+import { SafeProp } from "..";
+import mockConsole from 'jest-mock-console';
 
-test('Safe mode', () => {
+test('Default mode, default return type', () => {
+  const testObj = {
+    a: {
+      b: {
+        c: {
+          d: 'apple',
+        }
+      }
+    },
+    e: null,
+    f: undefined,
+  };
+  const sp = new SafeProp();
+  expect(sp.set(testObj).f('a').f('b').f('c').f('d').val === 'apple');
+  expect(sp.set(testObj).get('a.b.c.d').val === 'apple');
+  expect(sp.set(testObj).get('b.c.d').val === undefined);
+  expect(sp.set(testObj).get('e').val === null);
+  expect(sp.set(testObj).get('f').val === undefined);
+})
+
+test('Default mode, null return type', () => {
+  const testObj = {
+    a: {
+      b: {
+        c: {
+          d: 'apple',
+        }
+      }
+    },
+    e: null,
+  };
+  const sp = new SafeProp('default', 'null');
+  expect(sp.set(testObj).f('a').f('b').f('c').f('d').val === 'apple');
+  expect(sp.set(testObj).get('a.b.c.d').val === 'apple');
+  expect(sp.set(testObj).get('b.c.d').val === null);
+  expect(sp.set(testObj).get('e.c.d').val === null);
+  expect(sp.set(undefined).get('e.c.d').val === null);
+  expect(sp.set(null).get('e.c.d').val === null);
+});
+
+test('Default mode, generic return type', () => {
+  const testObj = {
+    a: {
+      b: {
+        c: {
+          d: 'apple',
+        }
+      }
+    },
+    e: null,
+  };
+  const sp = new SafeProp('default', 'generic');
+  expect(sp.set(testObj).f('a').f('b').f('c').f('d').val === 'apple');
+  expect(sp.set(testObj).get('a.b.c.d').val === 'apple');
+  expect(sp.set(testObj).get('b.c.d').val === undefined);
+  expect(sp.set(undefined).get('b.c.d').val === undefined);
+  expect(sp.set(testObj).get('e.c.d').val === null);
+  expect(sp.set(null).get('e.c.d').val === null);
+});
+
+test('Safe mode: valid property', () => {
   const testObj = {
     a: {
       b: {
@@ -15,20 +76,34 @@ test('Safe mode', () => {
   expect(sp.set(testObj).get('a.b.c.d').val === 'apple');
 });
 
-test('Safe mode: try access prop from undefined', () => {
+test('Safe mode: try access prop of undefined', () => {
   const testObj = {
     a: undefined
   }
+  const restoreConsole = mockConsole();
   const sp = new SafeProp('safe');
   expect(sp.set(testObj).f('a').f('b').f('c').f('d').val === undefined);
+  expect(sp.set(null).f('a').f('b').f('c').f('d').val === null);
+  expect(console.error).toHaveBeenCalled();
+  restoreConsole();
 });
 
-test('Default mode: try access prop from undefined', () => {
+test('Strict mode: try access prop of undefined', () => {
   const testObj = {
     a: undefined
   }
-  const sp = new SafeProp();
-  expect(() => {sp.set(undefined).f('a')}).toThrowError('safeProp Error: Cannot read property a from undefined!')
-  expect(() => {sp.set(testObj).f('a').f('b').f('c').f('d')}).toThrowError('safeProp Error: Cannot read property b from undefined!');
-  expect(() => {sp.set(testObj).get('a.b.c.d')}).toThrowError('safeProp Error: Cannot read property b from undefined!');
+  const sp = new SafeProp('strict');
+  expect(() => {sp.set(undefined).f('a')}).toThrowError('safeProp Error: Cannot read property a of undefined!')
+  expect(() => {sp.set(testObj).f('a').f('b').f('c').f('d')}).toThrowError('safeProp Error: Cannot read property b of undefined!');
+  expect(() => {sp.set(testObj).get('a.b.c.d')}).toThrowError('safeProp Error: Cannot read property b of undefined!');
+})
+
+test('Strict mode: try access prop of null', () => {
+  const testObj = {
+    a: null
+  }
+  const sp = new SafeProp('strict');
+  expect(() => {sp.set(null).f('a')}).toThrowError('safeProp Error: Cannot read property a of null!')
+  expect(() => {sp.set(testObj).f('a').f('b').f('c').f('d')}).toThrowError('safeProp Error: Cannot read property b of null!');
+  expect(() => {sp.set(testObj).get('a.b.c.d')}).toThrowError('safeProp Error: Cannot read property b of null!');
 })
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,63 @@
 export class SafeProp {
   public val: any;
-  private obj: object;
-  private mode: string;
-  constructor(mode?: string) {
-    if (mode === 'safe') {
-      this.mode = mode;
-    } else {
-      this.mode = 'default';
+  public returnEmptyType: string;
+  public mode: string;
+  constructor(mode?: string, returnEmptyType?: string) {
+    switch(mode) {
+      case 'safe':
+      case 'strict':
+        this.mode = mode;
+        break;
+      default: 
+        this.mode = 'default';
+    }
+    switch(returnEmptyType) {
+      case 'generic':
+      case 'null':
+        this.returnEmptyType = returnEmptyType;
+        break;
+      default: 
+        this.returnEmptyType = 'undefined'
     }
   }
 
   // wrap the object
   public set(obj: object) {
-    this.obj = obj;
+    // this.obj = obj;
     this.val = obj;
     return this;
   }
 
   // fetch the value from property name
   public f(prop: string) {
-    if (this.obj === undefined) {
-      if (this.mode === 'safe') {
-        this.val = undefined;
-        return this;
-      } else {
-        throw new Error(`safeProp Error: Cannot read property ${prop} from undefined!`)
+    if (this.val === undefined || this.val === null) {
+      const emptyType = this.val === undefined ? 'undefined' : 'null';
+      const errorMsg = `safeProp Error: Cannot read property ${prop} of ${emptyType}!`;
+
+      // if the returnEmptyType is specified, set value to that type; if not, return itself
+      this.val = this.returnEmptyType === 'undefined' ? undefined :
+        this.returnEmptyType === 'null' ? null :
+          this.val;
+
+      if (this.mode === 'strict') {
+        throw new Error(errorMsg);
       }
+      if (this.mode === 'safe') {
+        console.error(errorMsg);
+      }
+      return this;
     }
-    this.val = this.obj[prop];
-    this.obj = this.obj[prop]
+    this.val = this.val[prop];
     return this;
   }
 
   // fetch value from a chain of properties
   // eg. 'request.body.message'
   public get(propChain: string) {
-    if (this.obj === undefined) {
-      if (this.mode === 'safe') {
-        this.val = undefined;
-        return this;
-      } else {
-        throw new Error(`safeProp Error: Cannot read property ${propChain} from undefined!`)
-      }
-    }
     const propArr = propChain.split('.');
-    propArr.map((prop) => {this.f(prop);})
+    for (const prop of propArr) {
+      this.f(prop);
+    }
     return this;
   }
 }
